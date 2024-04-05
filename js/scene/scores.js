fbdcd7311aff64f691fe9b1c5fee108c83600969
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2024-04-05 16:51:48 +0100

    Scores button switch between recent and high scores

--- js/scene/scores.js
+++ js/scene/scores.js
@@ -4,6 +4,9 @@ class Scores extends Phaser.Scene {
    }
 
    init() {
+      this.showHighScores = false;
+      this.numScoresToShow = 9;
+      this.toggleLabel = 'show High';
    }
 
    create() {
@@ -12,13 +15,30 @@ class Scores extends Phaser.Scene {
       this.add.text(displayWidth / 2, 80, 'Spying scores', { font: '48px Arial', color: '#000000' })
          .setOrigin(0.5);
 
-      allScores.forEach((score, index) => {
-         this.add.text(30, 130 + index * 50, `${index + 1}. Intel: ${allScores[index].intel}  Progress: ${allScores[index].progress}`, { font: '24px Arial', fill: '#000000' });
-      });
+      // highScores = allScores sorted by intel values
+      this.highScores = [...allScores].sort((a, b) => b.intel - a.intel);
+
+      // show either allScores or highScores based on toggle button
+      this.displayScores();
 
       this.makeMenuButton();
+      this.makeToggleButton();
       this.input.keyboard.on('keyup', this.anyKey, this);
    };
+
+   displayScores() {
+      // clear previous score texts
+      if (this.scoreTexts) {
+         this.scoreTexts.forEach(text => text.destroy());
+      }
+
+      let scores = this.showHighScores ? this.highScores : allScores;
+      scores = scores.slice(0, this.numScoresToShow);
+
+      this.scoreTexts = scores.map((score, index) => {
+         return this.add.text(30, 130 + index * 50, `${index + 1}. Intel: ${score.intel}  Progress: ${score.progress}`, { font: '24px Arial', fill: '#000000' });
+      });
+   }
 
    anyKey(event) {
       let code = event.keyCode;
@@ -31,5 +51,30 @@ class Scores extends Phaser.Scene {
       this.buttonMenu = new hudButton(this, 62, 30, 'placeholderButtonUp', 'placeholderButtonDown', 'Menu', () => {
          this.scene.start("Home");
       }, 1);
+   }
+
+   makeToggleButton() {
+      this.buttonToggle = new hudButton(this, displayWidth - 62, 30, 'placeholderButtonUp', 'placeholderButtonDown', this.toggleLabel, () => {
+         this.showHighScores = !this.showHighScores;
+         this.updateToggleLabel();
+         this.buttonToggle.buttonText.setText(this.toggleLabel);
+         this.displayScores();
+      }, 1);
+   }
+
+   // makeToggleButton() {
+   //    this.buttonToggle = new hudButton(this, displayWidth - 62, 30, 'placeholderButtonUp', 'placeholderButtonDown', this.toggleLabel, () => {
+   //       this.showHighScores = !this.showHighScores;
+   //       this.updateToggleLabel();
+   //       this.displayScores();
+   //    }, 1);
+   // }
+
+   updateToggleLabel() {
+      if (this.showHighScores) {
+         this.toggleLabel = 'show Recent';
+      } else {
+         this.toggleLabel = 'show High';
+      }
    }
 }
